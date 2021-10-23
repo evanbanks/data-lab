b8a88ed2af1bde46641c15a64cac8ff06cf848dc
Evan Banks <evan.banks@colorado.edu>
2021-10-23 14:11:15 -0600

    solved p9

--- bits.c
+++ bits.c
@@ -193,7 +193,8 @@ int isGreater(int x, int y) {
  *   Rating: 3 
  */
 int rotateLeft(int x, int n) {
-  return 0;
+  int mask = ~(0x1 << 31);
+  return (((x >> 1) & mask) >> (32 + ~n)) | (x << n);
 }
 /* 
  * greatestBitPos - return a mask that marks the position of the
